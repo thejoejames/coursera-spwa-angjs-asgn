99cbb5d6a8555d2d51b058d41979f3ea63bbedc4
Joe <thejoejames@gmail.com>
2016-10-04 04:45:04 -0400

    updates

--- assignments/assignment3/solution/app.js
+++ assignments/assignment3/solution/app.js
@@ -11,7 +11,7 @@
         var ddo = {
             templateUrl: 'loader/itemsloaderindicator.template.html',
             scope: {
-                foundItems: '<',
+                items: '<',
                 onRemove: '&'
             },
             controller: MenuListDirectiveController,
